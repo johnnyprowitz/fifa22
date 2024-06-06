b834124a2b97cf771be970465bdc9ac77a4ca9dd
johnathanprowitz <johpro24@bergen.org>
2024-06-06 08:39:25 -0400

    fifa

--- src/main/java/com/johpro/fifa/MatchController.java
+++ src/main/java/com/johpro/fifa/MatchController.java
@@ -1,10 +1,13 @@
 package com.johpro.fifa;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -14,8 +17,18 @@ public class MatchController {
     @Autowired
     private MatchRepo mr;
 
-    @GetMapping("/all")
+    private String jsonFile="src/main/resources/static/static.json";
+    private void saveToFile(List<Matches> matches){
+        ObjectMapper objectMapper= new ObjectMapper();
+        try(FileWriter fileWriter = new FileWriter(jsonFile)){
+            objectMapper.writeValue(fileWriter, matches);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    @GetMapping("/display")
     public List<Matches> getMatches(){
+        saveToFile(mr.findAll());
 
         return mr.findAll();
     }
